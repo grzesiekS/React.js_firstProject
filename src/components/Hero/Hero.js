fc3544e216af361feb0f7467ef2d7bfeb2a6d65c
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-12 11:40:02 +0200

    Add prop types for Hero and List class

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
 const Hero = props => (
     <header className={styles.component}>
@@ -7,5 +8,9 @@ const Hero = props => (
         <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}></img>
     </header>
 );
+
+Hero.propTypes = {
+    titleText: PropTypes.node.isRequired,
+}
 
 export default Hero;
