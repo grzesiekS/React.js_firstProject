8181d8028487b1be3f04d63777d05c5887d83999
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-12 16:51:42 +0200

    Create class Card and init it in the class Column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
 
 class Column extends React.Component {
     static propTypes = {
@@ -11,6 +12,7 @@ class Column extends React.Component {
         return (
             <section className={styles.component}>
                 <h3 className={styles.title}>{this.props.title}</h3>
+                <Card />
             </section>
         )
     }
