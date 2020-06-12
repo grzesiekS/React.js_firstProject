13d9295153f4df050d2e735fd5be242563f3775f
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-12 11:51:07 +0200

    Create class Column

---
+++ src/components/Column/Column.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Column.scss';
+
+class Column extends React.Component {
+    render() {
+        <section className={styles.component}>
+            <h3 className={styles.title}></h3>
+        </section>
+    }
+}
