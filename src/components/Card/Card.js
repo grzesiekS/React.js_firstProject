eb219789f857920f60792d389ee29b6f25a19e86
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-12 18:01:49 +0200

    Create card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './Card.scss';
+import PropTypes from 'prop-types';
 
 class Card extends React.Component {
+    static propTypes = {
+        title: PropTypes.string
+    }
+
     render() {
         return (
             <div className={styles.component}>
-                <p>Test</p>
+                <p>{this.props.title}</p>
             </div>
         )
     }
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,17 +2,25 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
+import {settings} from '../../data/dataStore';
 
 class Column extends React.Component {
+    state = {
+        card: this.props.cards || [],
+    }
+    
     static propTypes = {
-        title: PropTypes.string
+        title: PropTypes.string,
+        card: PropTypes.array,
     }
 
     render() {
         return (
             <section className={styles.component}>
                 <h3 className={styles.title}>{this.props.title}</h3>
-                <Card />
+                {this.state.card.map(({key, ...cardProps}) => (
+                    <Card key={key} {...cardProps}/>
+                ))}
             </section>
         )
     }
