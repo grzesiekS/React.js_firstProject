3cdf3dd09498841fc94fcff64e650fcab59c21a2
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-12 11:56:51 +0200

    Modify Culumn class code

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,8 +3,12 @@ import styles from './Column.scss';
 
 class Column extends React.Component {
     render() {
-        <section className={styles.component}>
-            <h3 className={styles.title}></h3>
-        </section>
+        return (
+            <section className={styles.component}>
+                <h3 className={styles.title}>Test</h3>
+            </section>
+        )
     }
 }
+
+export default Column
