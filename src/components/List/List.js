4565837cd554274051e27746e79cd997e6a3866b
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-12 12:03:19 +0200

    Create Column instance in List class

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import Column from '../Column/Column';
 import PropTypes from 'prop-types';
 
 class List extends React.Component {
@@ -20,6 +21,13 @@ class List extends React.Component {
         <Hero titleText={this.props.title} 
           imgSrc={this.props.imgSrc}
         />
+
+        <div className={styles.columns}>
+          <Column title='Work' />
+          <Column title='Home' />
+          <Column title='Groceries' />
+        </div>
+
         <div className={styles.description}>
           {this.props.children}
         </div>
