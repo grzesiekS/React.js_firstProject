dd9135208d264fa95f0aa3ae971f044e5ba9beea
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-18 18:20:24 +0200

    Change props and key name

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -7,8 +7,8 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
-  addColumn: title => dispatch(createActionAddCard({
-    listId: props.id,
+  addCard: title => dispatch(createActionAddCard({
+    columnId: props.id,
     title,
   })),
 });
