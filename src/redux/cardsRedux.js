789b6635ec954576d9fa1b33f2fc8729c0137fa1
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-20 19:56:04 +0200

    Add js code for drag and drop cards between lists

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -21,6 +21,7 @@ export default function reducer(state = [], action = {}) {
     case ADD_CARD:
       return [...state, action.payload];
     case MOVE_CARD: {
+      //[IN PROGRESS]
       const {id, src, dest} = action.payload;
       const targetCard = state.filter(card => card.id == id)[0];
       const targetColumnCards = state.filter(card => card.columnId == dest.columnId).sort((a, b) => a.index - b.index);
@@ -31,7 +32,7 @@ export default function reducer(state = [], action = {}) {
 
         return state.map(card => {
           const targetColumnIndex = targetColumnCards.indexOf(card);
-        
+
           if(targetColumnIndex > -1 && card.index != targetColumnIndex){
             return {...card, index: targetColumnIndex};
           } else {
@@ -39,7 +40,40 @@ export default function reducer(state = [], action = {}) {
           }
         });
       } else {
-        return state;
+        let sourceColumnCards = state.filter(card => card.columnId == src.columnId).sort((a, b) => a.index - b.index);
+
+        // remove card from sourceColumn
+        sourceColumnCards.splice(src.index, 1);
+        // add card to targetColumn
+        targetColumnCards.splice(dest.index, 0, targetCard);
+
+        console.log('sourceColumnCards:');
+        console.log(sourceColumnCards.map(card => `${card.index}, title: ${card.title}`));
+        console.log('targetColumnCards:');
+        console.log(targetColumnCards.map(card => `${card.index}, title: ${card.title}`));
+
+        return state.map(card => {
+          const targetColumnIndex = targetColumnCards.indexOf(card);
+
+          if(card == targetCard){
+            // card is targetCard
+            return {...card, index: targetColumnIndex, columnId: dest.columnId};
+          } else if(targetColumnIndex > -1 && card.index != targetColumnIndex){
+            // card is in targetColumn
+            return {...card, index: targetColumnIndex};
+          } else {
+            // card is NOT in targetColumn
+            const sourceColumnIndex = sourceColumnCards.indexOf(card);
+
+            if(sourceColumnIndex > -1 && card.index != sourceColumnIndex){
+              // card is in sourceColumn
+              return {...card, index: sourceColumnIndex};
+            } else {
+              // card is NOT in sourceColumn (and NOT in targetColumn)
+              return card;
+            }
+          }
+        });
       }
     }
     default:
