0dd5b7fbeb506675766fee62f71099b712cd089b
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-20 15:57:15 +0200

    Implement Draggable component in Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
+import {Draggable} from 'react-beautiful-dnd';
 
 class Card extends React.Component {
     static propTypes = {
       title: PropTypes.string,
+      id: PropTypes.string,
+      index: PropTypes.number,
     }
 
     render() {
-      const {title} = this.props;
+      const {title, id, index} = this.props;
 
       return (
-        <div className={styles.component}>
-          <p className={styles.subtitle}>{title}</p>
-        </div>
+        <Draggable draggableId={id} index={index}>
+          {(provided) => (
+            <article
+              className={styles.component}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              ref={provided.innerRef}
+            >
+              <p className={styles.subtitle}>{title}</p>
+            </article>
+          )}
+        </Draggable>
       );
     }
 }
