03ebabbd796dc1499d955c6ddb506b86337759b0
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-20 16:19:37 +0200

    Add new action in cardsRedux

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -9,15 +9,19 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const ADD_CARD = createActionName('ADD_CARD');
+export const MOVE_CARD = createActionName('MOVE_CARD');
 
 // action creators
 export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
+export const createAction_moveCard = payload => ({payload: { ...payload }, type: MOVE_CARD });
 
 // reducer
 export default function reducer(state = [], action = {}) {
   switch (action.type) {
     case ADD_CARD:
       return [...state, action.payload];
+    case MOVE_CARD:
+      return[...state, action.payload];
     default:
       return state;
   }
