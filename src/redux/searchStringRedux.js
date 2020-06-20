def1ffa186ee09fbcd40cee80ef3bac31c49593a
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-20 11:02:58 +0200

    Create action name and action type in searchStringRedux.js

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -4,8 +4,11 @@ export const countAllCards = ({cards}) => cards.length;
 export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
 
