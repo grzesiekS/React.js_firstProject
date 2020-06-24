84dfeb84e54b64cde0ca23e5afee97082a5109f9
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-24 20:53:04 +0200

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {NavLink, Link} from 'react-router-dom';
+import styles from './Header.scss';
+import Container from '../Container/Container';
+
+const Header = () => (
+  <header className={styles.components}>
+    <Container>
+      <div className={styles.wrapper}>
+        <Link className={styles.logo} to='/'>
+          <i className='fas fa-bug'></i>
+        </Link>
+        <nav>
+          <NavLink exact to='/'>Home</NavLink>
+          <NavLink exact to='/info'>Info</NavLink>
+        </nav>
+      </div>
+    </Container>
+  </header>
+);
+
+export default Header;
