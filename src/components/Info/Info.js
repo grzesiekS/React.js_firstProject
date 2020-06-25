12fc12a201566d68224d4a45cbf556d06670e267
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-25 17:33:09 +0200

    Modify Info component (Add styles, add Hero comp)

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
+import styles from './Info.scss';
+import {infoContent} from '../../data/dataStore';
 
 const Info = () => (
   <Container>
-    <h2>Info</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <Hero titleText={infoContent.title} imgSrc={infoContent.image} />
+    <h2 className={styles.subtitle}>{infoContent.subtitle}</h2>
+    <p className={styles.content}>{infoContent.content}</p>
   </Container>
 );
 
