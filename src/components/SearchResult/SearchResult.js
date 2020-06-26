174ebeb3f5960a2098cdadd04ad48beffa75ae81
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-26 20:31:51 +0200

    Add basic jsx code in SearchResult component

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './SearchResult.scss'; 
+import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
+// import Card from '../Card/Card';
+import {searchResultContent} from '../../data/dataStore';
 
 class SearchResult extends React.Component {
+  static propTypes = {
+    searchString: PropTypes.string,
+  }
+
   render() {
-    return(false);
+    console.log(this);
+
+    return(
+      <Container>
+        <Hero titleText={searchResultContent.title} imgSrc={searchResultContent.image} />
+        <section className={styles.component}>
+          <h3 className={styles.title}>
+            <span className={styles.icon}><i className={searchResultContent.icon}></i></span>
+          </h3>
+
+          <div className={styles.cards}>
+          </div>
+        </section>
+      </Container>
+    );
   }
 }
 
