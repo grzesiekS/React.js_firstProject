23c6d44729de0fde2b3da85a735a9054ae1c0af5
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-26 22:37:02 +0200

    Add styles for Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,13 +11,13 @@ class Header extends React.Component {
       <header className={styles.components}>
         <Container>
           <div className={styles.wrapper}>
-            <Link className={styles.logo} to={navLinks.home.href}>
+            <Link className={[styles.link, styles.logo].join(' ')} to={navLinks.home.href}>
               <i className={settings.headerIcon}></i>
             </Link>
             <nav>
-              <NavLink exact to={navLinks.home.href} activeClassName='active'>{navLinks.home.name}</NavLink>
-              <NavLink exact to={navLinks.info.href} activeClassName='active'>{navLinks.info.name}</NavLink>
-              <NavLink exact to={navLinks.faq.href} activeClassName='active'>{navLinks.faq.name}</NavLink>
+              <NavLink exact to={navLinks.home.href} activeClassName='active' className={styles.link}>{navLinks.home.name}</NavLink>
+              <NavLink exact to={navLinks.info.href} activeClassName='active' className={styles.link}>{navLinks.info.name}</NavLink>
+              <NavLink exact to={navLinks.faq.href} activeClassName='active' className={styles.link}>{navLinks.faq.name}</NavLink>
             </nav>
           </div>
           <Search />
