38dc1887a9936e39ca929fdd79c8e45779431ffb
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-26 16:58:41 +0200

    Implement ListLink in Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from './Home.scss';
-import List from '../List/ListContainer';
+import ListLink from '../ListLink/ListLink';
 import PropTypes from 'prop-types';
-import Search from '../Search/SearchContainer';
 import {DragDropContext} from 'react-beautiful-dnd';
 
 class Home extends React.Component {
@@ -44,10 +43,9 @@ class Home extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
         <DragDropContext onDragEnd={moveCardHandler}>
           {lists.map(listData => (
-            <List key={listData.id} {...listData} />
+            <ListLink key={listData.id} {...listData} />
           ))}
         </DragDropContext>
       </main>
