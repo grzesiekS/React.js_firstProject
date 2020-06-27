3fa6ce915fc5613a05ac0a8f6dc0c2956f268446
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-06-27 09:22:18 +0200

    Implement react-beautiful-dnd

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
-// import {Draggable} from 'react-beautiful-dnd';
+import {Draggable} from 'react-beautiful-dnd';
 
 class Card extends React.Component {
     static propTypes = {
@@ -11,24 +11,24 @@ class Card extends React.Component {
     }
 
     render() {
-      const {title} = this.props;
+      const {title, id, index} = this.props;
 
       return (
-        // <Draggable draggableId={id} index={index}>
-        //   {(provided) => (
-        //     <article
-        //       className={styles.component}
-        //       {...provided.draggableProps}
-        //       {...provided.dragHandleProps}
-        //       ref={provided.innerRef}
-        //     >
-        //       <p className={styles.subtitle}>{title}</p>
-        //     </article>
-        //   )}
-        // </Draggable>
-        <article className={styles.component}>
-          <p className={styles.subtitle}>{title}</p>
-        </article>
+        <Draggable draggableId={id} index={index}>
+          {(provided) => (
+            <article
+              className={styles.component}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              ref={provided.innerRef}
+            >
+              <p className={styles.subtitle}>{title}</p>
+            </article>
+          )}
+        </Draggable>
+        // <article className={styles.component}>
+        //   <p className={styles.subtitle}>{title}</p>
+        // </article>
       );
     }
 }
